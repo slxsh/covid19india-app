a5d70d7400d63efac9c611a03bf24c11795fd264
Imtiaz Alam <44202585+slxsh@users.noreply.github.com>
2020-04-15 22:39:10 +0530

    Added Github repo link

--- About.js
+++ About.js
@@ -8,7 +8,7 @@ export default function About() {
             <Text style={styles.para}>This is not an official app. This app uses API provided by <Text style={{color:'#0377fc'}} onPress={() => Linking.openURL('https://github.com/covid19india')}>covid19india.org</Text> which uses crowdsourced patient database. The database is updated more frequently than MoHFW.</Text>
 
             <Text style={styles.para}>This app was made using React Native and it is open source. Feel free to contribute.</Text>
-            <Text style={{fontSize: 25, color:'#0377fc', fontFamily:'Teko-Bold', textAlign: 'center'}}>GITHUB</Text>
+            <Text style={{fontSize: 25, color:'#0377fc', fontFamily:'Teko-Bold', textAlign: 'center'}} onPress={() => Linking.openURL('https://github.com/slxsh/covid19india-app')}>GITHUB</Text>
             <Text style={{textAlign: 'center', fontFamily: 'Teko-Bold', fontSize: 30,}}>Stay Safe!</Text>
             <View style={{justifyContent: 'center', alignItems: 'center'}}>
                 <Image source={require('./corona.png')} style={{width:300, height: 300}} />
